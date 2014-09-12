0f134684ec06df6121122e676a106fe975100995
Udyant Wig <udyantw@gmail.com>
2014-09-13 00:32:28 +0530

    Improvements to the RNG and the shuffle

--- utilities.c
+++ utilities.c
@@ -9,18 +9,30 @@
 #include "utilities.h"
 
 int uniform_int_in (int min, int max)
-/* Pick a number in [MIN, MAX] with uniform probability. */
+/* Pick a number in [MIN, MAX] with uniform probability, where
+ *             0 <= min <= max <= 32767
+ * Adapted from randrange() by Richard Heathfield.
+ */
 {
-    return min + rand () % max;
+    if (min > max)
+    {
+	int t = min; min = max; max = t;
+    }
+    if (!(0 <= min && max <= 32767))
+	return -1;
+    return min + (max - min + 1) * (rand () / (RAND_MAX + 1.0));
 }
 
 void shuffle_int_array (int array [], int length)
-/* Shuffle the integer array of the given length. */
+/* Shuffle the integer array of the given length.
+ * NOTE: can only deal with arrays of maximum length 32768 on
+ *       account of the RNG.
+ */
 {
     int i, j, temp;
 
     for (i = 0; i < length; i++) {
-	j = rand () % (length - i);
+	j = uniform_int_in (i, length - 1);
 	temp = array [i];
 	array [i] = array [j];
 	array [j] = temp;
@@ -39,7 +51,7 @@ bool lookup (int array [], size_t length, int key)
 	    return true;
 
     return false;
-}    
+}
 
 bool alphabetic_numeric (const char *string)
 /* Is the given string made up only of alphabetic and/or numeric
@@ -62,9 +74,9 @@ bool alphabetic_numeric (const char *string)
     Synopsis: Performs an unsigned comparison of two strings without regard
     to the case of any letters in the strings.  Returns a value that is
     <TABLE>
-        <_0     if string1 is less than string2
-        ==_0    if string1 is equal to string2
-        >_0     if string1 is greater than string2
+    <_0     if string1 is less than string2
+    ==_0    if string1 is equal to string2
+    >_0     if string1 is greater than string2
     </TABLE>
     ---------------------------------------------------------------------[>]-*/
 
@@ -73,9 +85,9 @@ int lexcmp (const char *string1, const char *string2)
     int cmp;
 
     do
-      {
+    {
         cmp = (unsigned char) tolower (*string1) - (unsigned char) tolower (*string2);
-      }
+    }
     while (*string1++ && *string2++ && cmp == 0);
     return (cmp);
 }
