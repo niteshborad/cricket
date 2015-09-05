558b238ee620bbd41dbeee41a1826611b04e9355
Udyant Wig <udyant@gmail.com>
2015-09-05 20:53:40 +0530

    Sober up the layout.

--- commands.h
+++ commands.h
@@ -11,24 +11,25 @@ extern team *second;
 extern const int max_wickets;
 
 /* Commands */
-typedef struct {
+typedef struct
+{
     char *name;
-    void (*do_it) (void);
+    void (*do_it)(void);
 } niladic;
 
 
-typedef struct {
+typedef struct
+{
     char *name;
-    void (*set_aggression) (aggression aggr);
+    void (*set_aggression)(aggression aggr);
     aggression aggr;
 } monadic;
 
 
-niladic *lookup_niladic_command_name (char *command_name);
-void do_niladic_unless_match_underway (niladic *command);
-monadic *lookup_monadic_command_name (char *command_name);
-void do_monadic_unless_match_underway (monadic *command);
-void try_executing (char *command_name);
-
+niladic *lookup_niladic_command_name(char *command_name);
+void do_niladic_unless_match_underway(niladic *command);
+monadic *lookup_monadic_command_name(char *command_name);
+void do_monadic_unless_match_underway(monadic *command);
+void try_executing(char *command_name);
 
 #endif
