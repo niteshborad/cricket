7cffa173fd381db7cbee6a8b4349e4b3cb7e95bb
Udyant Wig <udyant@gmail.com>
2015-09-05 20:51:55 +0530

    Sober up the layout.

--- utilities.c
+++ utilities.c
@@ -8,62 +8,63 @@
 
 #include "utilities.h"
 
-int uniform_int_in (int min, int max)
+int uniform_int_in(int min, int max)
 /* Pick a number in [MIN, MAX] with uniform probability, where
  *             0 <= min <= max <= 32767
  * Adapted from randrange() by Richard Heathfield.
  */
 {
-    if (min > max)
-    {
-	int t = min; min = max; max = t;
-    }
-    if (!(0 <= min && max <= 32767))
-	return -1;
-    return min + (max - min + 1) * (rand () / (RAND_MAX + 1.0));
+  if (min > max)
+  {
+    int t = min; min = max; max = t;
+  }
+  if (!(0 <= min && max <= 32767))
+    return -1;
+  return min + (max - min + 1) * (rand() / (RAND_MAX + 1.0));
 }
 
-void shuffle_int_array (int array [], int length)
+void shuffle_int_array(int array[], int length)
 /* Shuffle the integer array of the given length.
  * NOTE: can only deal with arrays of maximum length 32768 on
  *       account of the RNG.
  */
 {
-    int i, j, temp;
+  int i, j, temp;
 
-    for (i = 0; i < length; i++) {
-	j = uniform_int_in (i, length - 1);
-	temp = array [i];
-	array [i] = array [j];
-	array [j] = temp;
-    }
+  for (i = 0; i < length; i++)
+  {
+    j = uniform_int_in(i, length - 1);
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 }    
 
-bool lookup (int array [], size_t length, int key)
+bool lookup(int array[], size_t length, int key)
 /* Search for KEY in ARRAY of size LENGTH.
  * Return true if found, else false.
  */
 {
-    size_t i;
+  size_t i;
 
-    for (i = 0; i < length; i++)
-	if (key == array [i])
-	    return true;
+  for (i = 0; i < length; i++)
+    if (key == array[i])
+      return true;
 
-    return false;
+  return false;
 }
 
-bool alphabetic_numeric (const char *string)
+bool alphabetic_numeric(const char *string)
 /* Is the given string made up only of alphabetic and/or numeric
  * characters?
  */
 {
-    const char *sp;
+  const char *sp;
 
-    for (sp = string; *sp != '\0'; sp++)
-	if (!isalnum (*sp))
-	    return false;
-    return true;
+  for (sp = string; *sp != '\0'; sp++)
+    if (!isalnum(*sp))
+      return false;
+  return true;
 }
 
 
@@ -82,12 +83,12 @@ bool alphabetic_numeric (const char *string)
 
 int lexcmp (const char *string1, const char *string2)
 {
-    int cmp;
+  int cmp;
 
-    do
-    {
-        cmp = (unsigned char) tolower (*string1) - (unsigned char) tolower (*string2);
-    }
-    while (*string1++ && *string2++ && cmp == 0);
-    return (cmp);
+  do
+  {
+    cmp = (unsigned char) tolower (*string1) - (unsigned char) tolower (*string2);
+  }
+  while (*string1++ && *string2++ && cmp == 0);
+  return (cmp);
 }
