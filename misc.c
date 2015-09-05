2ae16b1a8e92edbc01f4e4614370fecdf52ffa93
Udyant Wig <udyant@gmail.com>
2015-09-05 20:43:57 +0530

    Sober up the layout.

--- misc.c
+++ misc.c
@@ -3,43 +3,43 @@
 /*
  * Miscellaneous
  */
-void welcome (void)
+void welcome(void)
 {
-    puts ("\"It's not cricket.\"");
-    puts ("A diminutive cricket simulator.");
-    putchar ('\n');
-    puts ("Type 'h' for some help.");
+  puts("\"It's not cricket.\"");
+  puts("A diminutive cricket simulator.");
+  putchar ('\n');
+  puts("Type 'h' for some help.");
 }    
 
-void help (void)
+void help(void)
 {
-    puts ("    At the prompt '#', you can type any of the following commands.");
-    putchar ('\n');
-    puts ("    If there is no match in progress:");
-    puts ("q    - quit.");
-    puts ("h    - get this help on usage.");
-    puts ("nm   - begin a new match.");
-    putchar ('\n');
-    puts ("    If there is a match in progress:");
-    puts ("o    - play the next over, unless the innings has ended.");
-    puts ("pc   - tell something about the condition of the pitch.");
-    puts ("ma   - analyze the situation in the current innings with some statistics.");
-    puts ("s    - while the match is on, display the score for the team batting;");
-    puts ("       will show the scorecards once the match has ended.");
-    puts ("fow  - show the fall of wickets.");
-    puts ("cp   - show the current partnership.");
-    puts ("ci   - after the first innings completes, prepare for the next one.");
-    puts ("c n  - play with a balance between attack and defence.");
-    puts ("c a  - play aggressively while increasing the risk of wickets falling.");
-    puts ("c va - play very aggressively while further increasing risk of wickets falling.");
-    puts ("c d  - play defensively while decreasing the flow of runs.");
-    puts ("c vd - play very defensively while further decreasing the flow of runs.");
-    putchar ('\n');
-    puts ("    No other commands will be recognized.");
+  puts("    At the prompt '#', you can type any of the following commands.");
+  putchar('\n');
+  puts("    If there is no match in progress:");
+  puts("q    - quit.");
+  puts("h    - get this help on usage.");
+  puts("nm   - begin a new match.");
+  putchar('\n');
+  puts("    If there is a match in progress:");
+  puts("o    - play the next over, unless the innings has ended.");
+  puts("pc   - tell something about the condition of the pitch.");
+  puts("ma   - analyze the situation in the current innings with some statistics.");
+  puts("s    - while the match is on, display the score for the team batting;");
+  puts("       will show the scorecards once the match has ended.");
+  puts("fow  - show the fall of wickets.");
+  puts("cp   - show the current partnership.");
+  puts("ci   - after the first innings completes, prepare for the next one.");
+  puts("c n  - play with a balance between attack and defence.");
+  puts("c a  - play aggressively while increasing the risk of wickets falling.");
+  puts("c va - play very aggressively while further increasing risk of wickets falling.");
+  puts("c d  - play defensively while decreasing the flow of runs.");
+  puts("c vd - play very defensively while further decreasing the flow of runs.");
+  putchar('\n');
+  puts("    No other commands will be recognized.");
 }    
 
-void quit (void)
+void quit(void)
 {
-    exit (0);
+  exit(0);
 }
 
