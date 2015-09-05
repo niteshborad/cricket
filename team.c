70b6fed97e4c19b1aa190b89fdc89356e5f44d06
Udyant Wig <udyant@gmail.com>
2015-09-05 20:48:20 +0530

    Sober up the layout.

--- team.c
+++ team.c
@@ -12,137 +12,151 @@ const int max_wickets = MAX_WICKETS;
 /*
  * Teams
  */
-team *make_team (void)		/* Due to Richard Heathfield */
+team *make_team(void)		/* Due to Richard Heathfield */
 {
-    team blank = {0};
-    team *new_team = malloc (sizeof *new_team);
-    if (new_team != NULL) {
-	*new_team = blank;
-	new_team->max_wickets = max_wickets;
-	init_fall_of_wickets (new_team);
-    }
-    return new_team;
+  team blank = {0};
+  team *new_team = malloc(sizeof *new_team);
+  if (new_team != NULL)
+  {
+    *new_team = blank;
+    new_team->max_wickets = max_wickets;
+    init_fall_of_wickets(new_team);
+  }
+  return new_team;
 }
 
-void get_team_names (team *a, team *b)
+void get_team_names(team *a, team *b)
 {
-    char one [MAX_TEAM_NAME_SIZE], two [MAX_TEAM_NAME_SIZE];
-    char *nl;
-    bool found_newline_in_buffer = false;
-    size_t a_name_size, b_name_size;
-    bool duplicate_names = false;
+  char one[MAX_TEAM_NAME_SIZE], two[MAX_TEAM_NAME_SIZE];
+  char *nl;
+  bool found_newline_in_buffer = false;
+  size_t a_name_size, b_name_size;
+  bool duplicate_names = false;
 
-    do {
-	puts ("Enter the names of the teams playing on the next two lines.");
-	puts ("(Use only alphabets and digits, please.)");
-	while (fputs ("> ", stdout), fgets (one, MAX_TEAM_NAME_SIZE, stdin) != NULL) {
-	    nl = strchr (one, '\n');
-	    if (nl != NULL) {
-		*nl = '\0';
-		found_newline_in_buffer = true;
-	    }
-	    else
-		found_newline_in_buffer = false;
-	    if (alphabetic_numeric (one) == false) {
-		puts ("Please use only alphabets and digits.");
-		continue;
-	    }
-	    else
-		break;
-	}
-	if (found_newline_in_buffer == false)
-	    while (getchar () != '\n')
-		;
-    
-	while (fputs ("> ", stdout), fgets (two, MAX_TEAM_NAME_SIZE, stdin) != NULL) {
-	    nl = strchr (two, '\n');
-	    if (nl != NULL) {
-		*nl = '\0';
-		found_newline_in_buffer = true;
-	    }
-	    else
-		found_newline_in_buffer = false;
-	    if (alphabetic_numeric (two) == false) {
-		puts ("Please use only alphabets and digits.");
-		continue;
-	    }
-	    else
-		break;
-	}
-	if (found_newline_in_buffer == false)
-	    while (getchar () != '\n')
-		;
-
-	if (strcmp (one, two) != 0)
-	    duplicate_names = false;
-	else {
-	    puts ("A team playing against itself might be confusing.");
-	    puts ("Please do not do that.");
-	    putchar ('\n');
-	    duplicate_names = true;
-	}
-    } while (duplicate_names == true);
-
-    
-    errno = 0;
-    a_name_size = strlen (one) + 1;
-    a->name = malloc (a_name_size);
-    if (a->name == NULL) {
-	fprintf (stderr, "get_team_names: allocating space for a->name: %s\n", strerror (errno));
-	exit (1);
+  do {
+    puts("Enter the names of the teams playing on the next two lines.");
+    puts("(Use only alphabets and digits, please.)");
+    while (fputs("> ", stdout), fgets (one, MAX_TEAM_NAME_SIZE, stdin) != NULL)
+    {
+      nl = strchr(one, '\n');
+      if (nl != NULL)
+      {
+	*nl = '\0';
+	found_newline_in_buffer = true;
+      }
+      else
+	found_newline_in_buffer = false;
+      if (alphabetic_numeric(one) == false)
+      {
+	puts("Please use only alphabets and digits.");
+	continue;
+      }
+      else
+	break;
     }
-    strncpy (a->name, one, a_name_size - 1);
-    a->name [a_name_size - 1] = '\0';
-
-    errno = 0;
-    b_name_size = strlen (two) + 1;
-    b->name = malloc (b_name_size);
-    if (b->name == NULL) {
-	fprintf (stderr, "get_team_names: allocating space for b->name: %s\n", strerror (errno));
-	exit (1);
+    if (found_newline_in_buffer == false)
+      while (getchar() != '\n')
+	;
+    
+    while (fputs("> ", stdout), fgets(two, MAX_TEAM_NAME_SIZE, stdin) != NULL)
+    {
+      nl = strchr(two, '\n');
+      if (nl != NULL)
+      {
+	*nl = '\0';
+	found_newline_in_buffer = true;
+      }
+      else
+	found_newline_in_buffer = false;
+      if (alphabetic_numeric(two) == false)
+      {
+	puts("Please use only alphabets and digits.");
+	continue;
+      }
+      else
+	break;
     }
-    strncpy (b->name, two, b_name_size - 1);
-    b->name [b_name_size - 1] = '\0';
+    if (found_newline_in_buffer == false)
+      while (getchar() != '\n')
+	;
+
+    if (strcmp(one, two) != 0)
+      duplicate_names = false;
+    else
+    {
+      puts("A team playing against itself might be confusing.");
+      puts("Please do not do that.");
+      putchar('\n');
+      duplicate_names = true;
+    }
+  } while (duplicate_names == true);
+
+    
+  errno = 0;
+  a_name_size = strlen(one) + 1;
+  a->name = malloc(a_name_size);
+  if (a->name == NULL)
+  {
+    fprintf(stderr, "get_team_names: allocating space for a->name: %s\n", strerror(errno));
+    exit(1);
+  }
+  strncpy(a->name, one, a_name_size - 1);
+  a->name[a_name_size - 1] = '\0';
+
+  errno = 0;
+  b_name_size = strlen(two) + 1;
+  b->name = malloc(b_name_size);
+  if (b->name == NULL)
+  {
+    fprintf(stderr, "get_team_names: allocating space for b->name: %s\n", strerror(errno));
+    exit(1);
+  }
+  strncpy(b->name, two, b_name_size - 1);
+  b->name[b_name_size - 1] = '\0';
 }
 
-void init_fall_of_wickets (team *t)
+void init_fall_of_wickets(team *t)
 {
-    int i;
+  int i;
 
-    for (i = 1; i < max_players; i++) {
-	t->fall_of_wickets [i].runs_at_fall = -1;
-	t->fall_of_wickets [i].overs_at_fall = -1;
-	t->fall_of_wickets [i].balls_into_over = -1;
-    }
+  for (i = 1; i < max_players; i++)
+  {
+    t->fall_of_wickets[i].runs_at_fall = -1;
+    t->fall_of_wickets[i].overs_at_fall = -1;
+    t->fall_of_wickets[i].balls_into_over = -1;
+  }
 }
 
-void fall_of_wickets (team *t)
+void fall_of_wickets(team *t)
 {
-    int i;
-    fow fall;
+  int i;
+  fow fall;
     
-    if (t->wickets == 0) {
-	puts ("No wickets have fallen.");
-	return;
-    }
+  if (t->wickets == 0)
+  {
+    puts("No wickets have fallen.");
+    return;
+  }
 
-    /* Print half the list on one line */
-    for (i = 1; i <= t->max_wickets / 2; i++) {
-	fall = t->fall_of_wickets [i];
-	if (fall.runs_at_fall == -1)
-	    break;
-	printf ("%d/%d ", fall.runs_at_fall, i);
-	printf ("(%d.%d)  ", fall.overs_at_fall, fall.balls_into_over);
-    }
-    putchar ('\n');
+  /* Print half the list on one line */
+  for (i = 1; i <= t->max_wickets / 2; i++)
+  {
+    fall = t->fall_of_wickets[i];
+    if (fall.runs_at_fall == -1)
+      break;
+    printf("%d/%d ", fall.runs_at_fall, i);
+    printf("(%d.%d)  ", fall.overs_at_fall, fall.balls_into_over);
+  }
+  putchar('\n');
 
-    /* Print the other half on the next line */
-    for ( ; i < t->max_wickets; i++) {
-	fall = t->fall_of_wickets [i];
-	if (fall.runs_at_fall == -1)
-	    break;
-	printf ("%d/%d ", fall.runs_at_fall, i);
-	printf ("(%d.%d)  ", fall.overs_at_fall, fall.balls_into_over);
-    }
-    putchar ('\n');
+  /* Print the other half on the next line */
+  for ( ; i < t->max_wickets; i++)
+  {
+    fall = t->fall_of_wickets[i];
+    if (fall.runs_at_fall == -1)
+      break;
+    printf("%d/%d ", fall.runs_at_fall, i);
+    printf("(%d.%d)  ", fall.overs_at_fall, fall.balls_into_over);
+  }
+  putchar('\n');
 }    
