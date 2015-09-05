9f892c639813a2b6b20f5229479a80715d58f5a1
Udyant Wig <udyant@gmail.com>
2015-09-05 20:52:39 +0530

    Sober up the layout.

--- analysis.h
+++ analysis.h
@@ -17,15 +17,15 @@ extern int which_innings;
 extern int target;
 
 /* Match analysis */
-double runrate (int runs, int overs);
-void scoreline (team *t);
-void projected_score (void);
-int runs_in_boundaries (team *t);
-double percentage_runs_in_boundaries (team *t);
-void match_analysis (void);
-void print_fall_of_wickets (void);
-void current_partnership (void);
-void scorecard (void);
-void summarize_match (void);
+double runrate(int runs, int overs);
+void scoreline(team *t);
+void projected_score(void);
+int runs_in_boundaries(team *t);
+double percentage_runs_in_boundaries(team *t);
+void match_analysis(void);
+void print_fall_of_wickets(void);
+void current_partnership(void);
+void scorecard(void);
+void summarize_match(void);
 
 #endif
