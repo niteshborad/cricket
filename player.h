fb29b6b0febc21de6efb5ead9528fe4e5e0583fb
Udyant Wig <udyant@rudiments.goosenet.in>
2016-06-29 11:02:33 +0530

    Add attributes of a player.

--- player.h
+++ player.h
@@ -3,7 +3,20 @@
 
 typedef struct
 {
-
+  char *name;
+  int runs_scored;
+  int balls;
+  double strike_rate;
+  int fours;
+  int sixes;
+  char *how_out;
+  char *fow;
+  int overs;
+  int maidens;
+  int runs_conceded;
+  int wickets;
+  double econ_rate;
+  bool is_keeper;
 } player;
 
 #endif
