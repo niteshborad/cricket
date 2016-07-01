38b96b778c934b9a7770405f837d9e10679b120a
Udyant Wig <udyantw@gmail.com>
2016-07-01 19:25:58 +0530

    Drop monadic commands; make all niladic.

--- commands.c
+++ commands.c
@@ -17,7 +17,6 @@ extern const int max_wickets;
 
 extern bool match_under_way;
 
-
 /*
  * Commands
  */
@@ -32,17 +31,13 @@ niladic niladic_commands[] =
   {"fow", print_fall_of_wickets},
   {"pc", show_pitch_condition},
   {"ci", change_innings},
+  {"c n", change_aggression_normal},
+  {"c a", change_aggression_aggressive},
+  {"c d", change_aggression_defensive},
+  {"c va", change_aggression_very_aggressive},
+  {"c vd", change_aggression_very_defensive},
+  {"sb", set_bowler},
   {NULL, NULL}
-};
-
-monadic monadic_commands[] =
-{
-  {"c n", change_aggression, NORMAL},
-  {"c a", change_aggression, AGGRESSIVE},
-  {"c d", change_aggression, DEFENSIVE},
-  {"c va", change_aggression, VAGGRESSIVE},
-  {"c vd", change_aggression, VDEFENSIVE},
-  {NULL, NULL, -1}
 };
 
 niladic *lookup_niladic_command_name(char *command_name)
@@ -70,36 +65,10 @@ void do_niladic_unless_match_underway(niladic *command)
   }
 }
 
-monadic *lookup_monadic_command_name(char *command_name)
-{
-  monadic *mp;
-  for (mp = monadic_commands; mp->name != NULL; mp++)
-  {
-    if (strcmp(command_name, mp->name) == 0)
-    {
-      return mp;
-    }
-  }
-  return NULL;
-}
-
-void do_monadic_unless_match_underway(monadic *command)
-{
-  if (match_under_way)
-  {
-    command->set_aggression(command->aggr);
-  }
-  else
-  {
-    puts("There is no match in progress yet.");
-  }
-}
-
 void try_executing(char *command_name)
 {
   niladic *ncmd;
-  monadic *mcmd;
-
+  
   if (strcmp("nm", command_name) == 0 ||
       strcmp("q", command_name) == 0 ||
       strcmp("h", command_name) == 0)
@@ -109,10 +78,6 @@ void try_executing(char *command_name)
   else if ((ncmd = lookup_niladic_command_name(command_name)) != NULL)
   {
     do_niladic_unless_match_underway(ncmd);
-  }
-  else if ((mcmd = lookup_monadic_command_name(command_name)) != NULL)
-  {
-    do_monadic_unless_match_underway(mcmd);
   }
   else
   {
--- commands.h
+++ commands.h
@@ -10,19 +10,8 @@ typedef struct
   void (*do_it)(void);
 } niladic;
 
-
-typedef struct
-{
-  char *name;
-  void (*set_aggression)(aggression aggr);
-  aggression aggr;
-} monadic;
-
-
 niladic *lookup_niladic_command_name(char *command_name);
 void do_niladic_unless_match_underway(niladic *command);
-monadic *lookup_monadic_command_name(char *command_name);
-void do_monadic_unless_match_underway(monadic *command);
 void try_executing(char *command_name);
 
 #endif
